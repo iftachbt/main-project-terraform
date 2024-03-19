42bee0fcdc2d5cf1fab0048e138ebcf192c715eb
iftachbt <iftachbt@gmail.com>
2024-03-19 19:53:40 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "iftach_ec2" {
+  ami           = "ami-080e1f13689e07408"
+  instance_type = "t2.micro"
+  key_name      = "lab-web"
+  # subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+
+  user_data = <<-EOF
+#!/bin/bash
+echo "Installing dependencies..."
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+echo "Pulling and running Docker container..."
+sudo docker pull iftachbt/sumimage:latest
+sudo docker run -d -p 5000:5000 iftachbt/sumimage:latest
+EOF
+}
+output "public_ip" {
+  value = aws_instance.iftach_ec2.public_ip
+}
---
+++ security-group.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "my_security_group" {
+  # vpc_id      = aws_vpc.main.id
+  name        = "drsgon-rf-sg"
+  description = "My custom security group for devops course"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound all traffic rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
